2d10b17efa7ae80683db3ff31b89c1e2da93db52
Jacek Ewertowski <jacek.ewertowski@allegro.pl>
2018-11-05 16:02:30 +0100

    Migrate offer-store from Java to Kotlin

--- offer-search-service/src/main/java/pl/allezon/offersearch/domain/Offer.kt
+++ offer-search-service/src/main/java/pl/allezon/offersearch/domain/Offer.kt
@@ -3,8 +3,33 @@ package pl.allezon.offersearch.domain
 import org.springframework.data.elasticsearch.annotations.Document
 
 @Document(indexName = "offer")
-data class Offer(
-        val id: Long,
-        val name: String,
-        val description: String
-)
+open class Offer {
+    var id: Long? = null
+    var name: String? = null
+    var description: String? = null
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Offer
+
+        if (id != other.id) return false
+        if (name != other.name) return false
+        if (description != other.description) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id?.hashCode() ?: 0
+        result = 31 * result + (name?.hashCode() ?: 0)
+        result = 31 * result + (description?.hashCode() ?: 0)
+        return result
+    }
+
+    override fun toString(): String {
+        return "Offer(id=$id, name=$name, description=$description)"
+    }
+
+}
